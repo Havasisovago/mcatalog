521f9707be84ac1c9c9a6b7d7e0f9d81099ff959
Kovacs Gabor <kovacs0gabor1999@gmail.com>
2023-04-24 08:36:31 +0200

    second

---
+++ muzic-catalog/src/main/java/hu/nye/progkor/muziccatalog/MuzicCatalogApplication.java
@@ -0,0 +1,13 @@
+package hu.nye.progkor.muziccatalog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MuzicCatalogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MuzicCatalogApplication.class, args);
+	}
+
+}
---
+++ muzic-catalog/src/test/java/hu/nye/progkor/muziccatalog/MuzicCatalogApplicationTests.java
@@ -0,0 +1,13 @@
+package hu.nye.progkor.muziccatalog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MuzicCatalogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
